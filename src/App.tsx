f8af26cdd4606716131118826fdea13ea5f31ef8
arashjp8 <arashjafarpour723@gmail.com>
2025-05-18 14:05:19 +0330

    checkpoint: loading neshan map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { MapComponent, MapTypes } from "@neshan-maps-platform/mapbox-gl-react";
+import "@neshan-maps-platform/mapbox-gl/dist/NeshanMapboxGl.css";
+
 function App() {
-  return (
-    <div
-      className={
-        "w-full min-h-screen flex justify-center items-center text-white text-3xl bg-gray-900"
-      }
-    >
-      mooooo 🐮
-    </div>
-  );
+    return (
+        <MapComponent
+            options={{
+                mapKey: import.meta.env.VITE_NESHAN_API_KEY,
+                mapType: MapTypes.neshanRaster,
+                zoom: 11,
+                center: [51.389, 35.6892],
+            }}
+        />
+    );
 }
 
 export default App;
